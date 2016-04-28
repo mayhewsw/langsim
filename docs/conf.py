a1ab2ab4db1ffeeaa5bdd7d02c0023327591d42a
Stephen Mayhew <mayhew2@illinois.edu>
2016-04-28 16:37:07 -0500

    updated to work better?

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import os
 import shlex
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.interpolate', 'scipy.spatial.distance', 'wikidatastats', 'uriel','langsim','phoible', 'wals']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
--- docs/uriel.rst
+++ docs/uriel.rst
@@ -5,6 +5,7 @@ URIEL is important -- a collection of Phoible and WALS.
 
 See URIEL here: `URIEL <http://www.cs.cmu.edu/~dmortens/uriel.html>`_
 
+Once you download uriel, put it in the `data/` folder, and update the parameter in uriel.py
 
 Module Documentation
 ---------------------
